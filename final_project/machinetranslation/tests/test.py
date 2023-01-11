28ea378f76707b742bf0095b6c203793426e048c
Sebestyen Ivanka-Toth <ivankaths@gmail.com>
2023-01-11 13:42:54 +0100

    Editing test.py

--- final_project/machinetranslation/tests/test.py
+++ final_project/machinetranslation/tests/test.py
@@ -5,12 +5,14 @@ from translator import english_to_french, french_to_english
 class TestEnglishToFrench(unittest.TestCase):
     def test_english_to_french(self):
         self.assertEqual(english_to_french('Hello'), 'Bonjour')
+        self.assertNotEqual(english_to_french(0), 'Bonjour')
         self.assertEqual(english_to_french('What is your name?'), 'Quel est votre nom?')
 
 
 class TestFrenchToEnglish(unittest.TestCase):
     def test_french_to_english(self):
         self.assertEqual(french_to_english('Bonjour'), 'Hello')
+        self.assertNotEqual(french_to_english(0), 'Hello')
         self.assertEqual(french_to_english('Quelle heure est-il?'), 'What time is it?')
 
 
